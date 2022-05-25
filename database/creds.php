d48bff60a82d8919452137d7bc930db696ffca0d
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2022-05-24 22:04:57 -0400

    Database SQL

--- database/creds.php
+++ database/creds.php
@@ -1,14 +1,18 @@
+<p>
 <?php
-    $db = parse_url(getenv("DATABASE_URL"));
-    $pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-    ));
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
 
-    $stmt = $pdo->query("SELECT * FROM accounts");
-?>
-<p><?php $stmt ?></p>
+$sql = $pdo->prepare("SELECT * FROM accounts WHERE id=1");
+$sql->execute();
+while ($result = $sql->fetch(PDO::FETCH_ASSOC)) {
+    echo $result['username']."<br/>";
+}
+?></p>
