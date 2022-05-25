0dcf419c6c13543c5a7a9fda4521c9a314840338
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2022-05-24 20:34:42 -0400

    Tile Layout + Videos

--- pages/indivBrawlerPage.php
+++ pages/indivBrawlerPage.php
@@ -3,14 +3,43 @@
 <?php include("../api/specificBrawlerDataAPI.php") ?>
 
 <body>
-    <link rel="stylesheet" href="/CSS/body.css" type="text/css">
-    <div class="container content has-text-centered" style="height: 95%;">
-        <h2 class="title is-2">
-            <?php
-            print_r($brawlerData);
-            ?>
-        </h2>
+    <div class="container" style="padding-top: 20px;">
+        <div class="tile is-ancestor is-vertical">
+            <div class="tile is-parent">
+                <article class="tile is-3 is-child notification is-info">
+                    <p class="title"><?php echo $brawlerData["name"] ?></p>
+                    <img src=<?php echo $brawlerData["imageUrl"] ?> width="150px" height="150px">
+                </article>
 
+                <article class="tile is-child notification">
+                    <p class="title">About the Brawler</p>
+                    <p><?php echo $brawlerData["description"]?></p>
+                    <p style="padding-top: 10px;">Rarity: <?php echo $brawlerData["rarity"]["name"]?></p>
+                    <p>Class: <?php echo $brawlerData["class"]["name"]?></p>
+                </article>
+            </div>
+
+            <div class="tile is-parent">
+                <?php
+                if ($brawlerData["videos"] != null) {
+                    echo '<article class="tile is-child">';
+                    echo '<p class="title">' . $brawlerData["name"] . '\'s Main Attack</p>';
+                    echo '<video width="250" height="150" controls>';
+                    echo '<source src=' . $brawlerData["videos"][0]["videoUrl"] . '>';
+                    echo '</video>';
+                    echo '</article>';
+
+                    echo '<article class="tile is-child">';
+                    echo '<p class="title">' . $brawlerData["name"] . '\'s Super Attack</p>';
+                    echo '<video width="250" height="150" controls>';
+                    echo '<source src=' . $brawlerData["videos"][1]["videoUrl"] . '>';
+                    echo '</video>';
+                    echo '</article>';
+                }
+                ?>
+            </div>
+
+        </div>
     </div>
 </body>
 
